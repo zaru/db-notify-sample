bc1c8dbd152de2387b657952c906f35338ff0350
zaru <sakuraba@basicinc.jp>
2017-06-06 17:59:18 +0900

    Added RethinkDB EventEmitter sample code

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+(()=>{
+  const r = require('rethinkdb')
+  r.connect({
+    host: "rethinkdb",
+    port: 28015,
+    db: 'test'
+  }, function(err, conn) {
+    console.log("Error: " + err)
+
+    r.table('sample').filter({}).run(conn, (err, cursor) => {
+      console.log("Error: " + err)
+
+      cursor.toArray( (err, result) => {
+        console.log("Error: " + err)
+        console.log(JSON.stringify(result, null, 2));
+      })
+    });
+
+    r.table("sample").changes().filter({}).run(conn, (err, cursor) => {
+      cursor.on("data", (message) => {
+        console.log(message)
+      })
+    })
+  })
+
+})()
+
 const redis = require("redis"),
       client = redis.createClient({ host: "redis", port: 6379 })
 
